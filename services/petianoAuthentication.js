6cd55dd58ea4720c46a656e5b7359c2ef4967230
dgmneto <dgmn@cin.ufpe.br>
2017-02-25 17:21:15 -0300

    resolving simple syntax error

--- services/petianoAuthentication.js
+++ services/petianoAuthentication.js
@@ -48,7 +48,7 @@ module.exports = (req, res, next) => {
 			.catch((err) => {
 				res.status(500);
 				console.log(err);
-				res.send({message: 'Erro Interno');
+				res.send({message: 'Erro Interno'});
 			});
 	} else {
 		res.status(403);
