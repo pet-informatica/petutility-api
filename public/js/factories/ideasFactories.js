57368bd687226e938a736d2b12a8007c3b76fc27
Pedro Rossi <pgrr@cin.ufpe.br>
2017-04-28 09:37:27 -0300

    Ideias refactored

--- public/js/factories/ideasFactories.js
+++ public/js/factories/ideasFactories.js
@@ -1,29 +1,24 @@
 angular
 	.module('Ideas')
 	.factory('IdeasAPI', function($resource, Request) {
-			var API = {
-				addIdea: Request.send('post', $resource('/api/ideas/create')),
-				getIdeas: Request.send('get', $resource('/api/ideas/')),
-				deleteIdea: Request.send('delete', $resource('/api/ideas/:ideaId/delete', {ideaId: '@id'})),
-				updateIdea: Request.send('post', $resource('/api/ideas/:ideaId/update', {ideaId: '@IdeaId'})),
-			};
+			var API = $resource('/api/ideas/:ideaId', {}, {
+				'update': {
+					method: 'PUT'
+				}
+			});
 
-			return {  
+			return {
 				getIdeas: (done) => {
 					API
-						.getIdeas()
-						.then(function(data) {
+						.query(function(data) {
 							return done && done(null, data);
 						}, function(err) {
 							return done && done(err);
 						});
 				},
 				addIdea: (title, description, done) => {
-					console.log(API.getIdeas)
-					console.log(API.addIdea)
 					API
-						.addIdea({Title: title, Description: description})
-						.then(function(data) {
+						.save({Title: title, Description: description}, function(data) {
 							return done && done(null, data);
 						}, function(err) {
 							return done && done(err);
@@ -31,17 +26,15 @@ angular
 				},
 				updateIdea: (ideaId, title, description, done) => {
 					API
-						.updateIdea({IdeaId: ideaId, Title: title, Description: description})
-						.then(function(data) {
+						.update({ideaId: ideaId}, {Title: title, Description: description}, function(data) {
 							return done && done(null, data);
 						}, function(err) {
 							return done && done(err);
-						})
+						});
 				},
 				deleteIdea: (ideaId, done) => {
 					API
-						.deleteIdea({ideaId: ideaId})
-						.then(function(data) {
+						.delete({ideaId: ideaId}, function(data) {
 							return done && done(null, data);
 						}, function(err) {
 							return done && done(err);
