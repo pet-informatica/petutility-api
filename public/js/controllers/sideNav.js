5a2f0982dce478c1dc77c078ad846386a83b0579
dgmneto <dgmneto@hotmail.com>
2017-07-19 21:13:35 -0300

    removing frontend

--- public/js/controllers/sideNav.js
+++
@@ -1,8 +0,0 @@
-angular
-	.module('SideNav')
-	.controller('SideNavController', function($scope, UserAPI, CandidatoAPI) {
-		$scope.logout = function() {
-			UserAPI.logout();
-			CandidatoAPI.logout();
-		};
-	});
